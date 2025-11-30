6f2cecaf580c1189f3b54fa5f6426081e6f59010
Jacob-Hatanaka <jacobth@hawaii.edu>
2025-11-30 13:20:02 -1000

    Add files via upload

---
+++ Gather.cs
@@ -0,0 +1,59 @@
+using Godot;
+using System;
+
+public partial class Gather : Control
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+        GridContainer grid = new GridContainer();
+        grid.Position = new Vector2(960, 0);
+        grid.SizeFlagsHorizontal = SizeFlags.ExpandFill;
+        AddChild(grid);
+        var woodButton = new GatherResource("Wood", 1);
+        grid.AddChild(woodButton);
+        var stoneButton = new GatherResource("Stone", 1);
+        grid.AddChild(stoneButton);
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
+
+public partial class GatherResource : Button
+{
+    private static bool disabled = false;
+    private string resourceName;
+    private int resourceAmount;
+    public GatherResource(string resourceName, int resourceAmount)
+    {
+        this.resourceAmount = resourceAmount;
+        this.resourceName = resourceName;
+    }
+
+    private Inventory inventory;
+
+    public override void _Ready()
+    {
+        inventory = GetNode<Inventory>("/root/Main/TabContainer/Inventory");
+        this.Text = $"Gather {resourceAmount} {resourceName}";
+    }
+
+    public override void _Pressed()
+    {
+        if (disabled) return;
+        // add 1 sec time to collect
+        disabled = true;
+        Timer timer = new Timer();
+        timer.WaitTime = 1.0;
+        timer.OneShot = true;
+        timer.Timeout += () => {
+            inventory.addItem(resourceName, resourceAmount);
+            disabled = false;
+        };
+        AddChild(timer);
+        timer.Start();
+    }
+}
---
+++ Inventory.cs
@@ -0,0 +1,69 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public partial class Inventory : Control
+{
+	public Dictionary<string, Item> items = new Dictionary<string, Item>();
+    private GridContainer grid;
+    public bool addItem(string itemName, int itemAmount)
+    {
+        if (items.ContainsKey(itemName))
+        {
+            items[itemName].addAmount(itemAmount);
+        }
+        else
+        {
+            items[itemName] = new Item(itemName, itemAmount);
+            grid.AddChild(items[itemName]);
+        }
+        return true;
+    }
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+        grid = new GridContainer();
+        grid.Position = new Vector2(960, 0);
+        grid.SizeFlagsHorizontal = SizeFlags.ExpandFill;
+        AddChild(grid);
+        foreach (var item in items)
+        {
+            grid.AddChild(item.Value);
+        }
+    }
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+
+	}
+}
+
+public partial class Item : HBoxContainer
+{
+    public string itemName;
+    public int itemAmount;
+    private Label nameLabel;
+    private Label amountLabel;
+
+    public Item(string itemName, int itemAmount)
+    {
+        this.itemName = itemName;
+        this.itemAmount = itemAmount;
+    }
+
+    public override void _Ready()
+    {
+        nameLabel = new Label();
+        nameLabel.Text = itemName;
+        AddChild(nameLabel);
+
+        amountLabel = new Label();
+        amountLabel.Text = itemAmount.ToString();
+        AddChild(amountLabel);
+    }
+    public void addAmount(int amt) { 
+        itemAmount += amt;
+        amountLabel.Text = itemAmount.ToString();
+    }
+}
